c63f143160d7fa256f4bf46a4025d55ade8d4694
nSA07 <nechaev1sergey@gmail.com>
2025-11-22 09:56:56 +0200

    fix env for image

--- src/app/[categorySlug]/[productSlug]/page.tsx
+++ src/app/[categorySlug]/[productSlug]/page.tsx
@@ -16,7 +16,8 @@ export async function generateMetadata({ params }: { params: Promise<{ categoryS
     };
   }
 
-  const baseUrl = "https://directus.fpvmaster.com.ua";
+  const baseUrl = process.env.NEXT_PUBLIC_ASSETS_URL;
+  const baseUrlImages = process.env.NEXT_PUBLIC_DIRECTUS_URL || baseUrl;
   const productUrl = `${baseUrl}/${categorySlug}/${product.slug}`;
 
   // Title
@@ -32,7 +33,7 @@ export async function generateMetadata({ params }: { params: Promise<{ categoryS
 
   // OG / Twitter image
   const image = product.images?.length
-    ? `${process.env.NEXT_PUBLIC_DIRECTUS_URL || baseUrl}/assets/${product.images[0].directus_files_id}`
+    ? `${process.env.NEXT_PUBLIC_DIRECTUS_URL || baseUrlImages}/assets/${product.images[0].directus_files_id}`
     : `${baseUrl}/og-default.jpg`;
 
   // Availability
